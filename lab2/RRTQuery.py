e2c1614a34949079eec088d9b84e92e5d6bc814f
Corinne Alini <coryalini@gmail.com>
2022-03-13 14:29:17 -0400

    Updating rrtquery to original

--- lab2/RRTQuery.py
+++ lab2/RRTQuery.py
@@ -60,100 +60,38 @@ rrtEdges=[]
 
 rrtVertices.append(qInit)
 rrtEdges.append(0)
-
-thresh=0.25
-FoundSolution=False
+thresh = 0.25
+FoundSolution = False
 
 while len(rrtVertices) < 3000 and not FoundSolution:
 	print(len(rrtVertices))
-	qRand = mybot.SampleRobotConfig()
-	if np.random.uniform(0, 1) < 0.01:
-		qRand = qGoal
-	idNear = FindNearest(rrtVertices, qRand)
-	qNear = rrtVertices[idNear]
-	while np.linalg.norm(np.array(qRand) - np.array(qNear)) > thresh:
-		subtracted_arr = np.array(qRand) - np.array(qNear)
-
-		qConnect = np.array(qNear) + (thresh * subtracted_arr)/np.linalg.norm(subtracted_arr)
-		if not mybot.DetectCollisionEdge(qConnect, qNear, pointsObs, axesObs):
-			rrtVertices.append(qConnect)
-			rrtEdges.append(idNear)
-			qNear = qConnect
-		else: 
-			break
-	# if np.linalg.norm(subtracted_arr) > thresh:
-	# 	qConnect = np.array(qNear) + (thresh * subtracted_arr)/np.linalg.norm(subtracted_arr)
-	# else:
-	qConnect = qRand
-	if not mybot.DetectCollisionEdge(qConnect, qNear, pointsObs, axesObs):
-		rrtVertices.append(qConnect)
-		rrtEdges.append(idNear)
-	idNear = FindNearest(rrtVertices, qGoal)
-	if np.linalg.norm(np.array(qGoal)-np.array(rrtVertices[idNear])) < 0.025:
-		rrtVertices.append(qGoal)
-		rrtEdges.append(idNear)
-		FoundSolution = True
-		break
-
 
 ### if a solution was found
-		
+
 if FoundSolution:
 	# Extract path
-	plan=[]
-	c=-1 #Assume last added vertex is at goal 
+	plan = []
+	c = -1  # Assume last added vertex is at goal
 	plan.insert(0, rrtVertices[c])
 
 	while True:
-		c=rrtEdges[c]
+		c = rrtEdges[c]
 		plan.insert(0, rrtVertices[c])
-		if c==0:
+		if c == 0:
 			break
 
 	# TODO - Path shortening
-	# for i in range(150):
-		# # pass
-		# anchorA = np.random.randint(0, len(plan) - 2)
-		# anchorB = np.random.randint(anchorA + 1, len(plan) - 1)
-		# shiftA = np.random.uniform(0,1)
-		# shiftB = np.random.uniform(0,1)
-		# candidateA = (1 - shiftA)* np.array(plan[anchorA]) + shiftA*np.array(plan[anchorA + 1])
-		# candidateB = (1 - shiftB)* np.array(plan[anchorB]) + shiftB*np.array(plan[anchorB + 1])
-		# if not mybot.DetectCollisionEdge(candidateA, candidateB, pointsObs, axesObs):
-		# 	while anchorB > anchorA:
-		# 		plan.pop(anchorB)
-		# 		anchorB = anchorB - 1
-		# 	plan.insert(anchorA + 1, candidateB)
-		# 	plan.insert(anchorA + 1, candidateA)
-	fa.reset_joints()
-	for joint in plan:
-		fa.goto_joints(joint)
-	# collision_boxes_publisher = CollisionBoxesPublisher('collision_boxes')
-	# rate = rospy.Rate(1)
-	# i = 0
-	# print(plan)
-	# while not rospy.is_shutdown():
-	# 	rate.sleep()
-	# 	time.sleep(2)
-	# 	joints = plan[i % len(plan)]
-	# 	mybot.PlotCollisionBlockPoints(joints, pointsObs)
+	for i in range(150):
+		pass
 
-	# 	fr.publish_joints(joints)
-	# 	fr.publish_collision_boxes(joints)
-	# 	collision_boxes_publisher.publish_boxes(boxes)
-	# 	i +=1
-		
-	# robot=vpi.vBot()
-	# robot.connect()
-	
-	# for q in plan:
-	# 	robot.move(q)
-	# 	time.sleep(1)
+	robot = vpi.vBot()
+	robot.connect()
 
-	# robot.destroy()
+	for q in plan:
+		robot.move(q)
+		time.sleep(1)
+
+	robot.destroy()
 
 else:
 	print("No solution found")
-
-
-
